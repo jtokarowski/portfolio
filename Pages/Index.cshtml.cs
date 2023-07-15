ff01b0134a03219dcad95e8fc00cec4a4f89c432
John Tokarowski <Johnt3@bu.edu>
2023-07-15 18:51:19 -0400

    initial commit for portfolio website

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace tokarowski_dev.Pages;
+
+public class IndexModel : PageModel
+{
+    public List<PortfolioItem> PortfolioItems { get; set; }
+
+    public void OnGet()
+    {
+        PortfolioItems = GetPortfolioItems(); // Method to retrieve your portfolio items
+    }
+
+    private List<PortfolioItem> GetPortfolioItems()
+    {
+        // Define and return a list of your portfolio items
+        return new List<PortfolioItem>
+        {
+            new PortfolioItem { Title = "Project 1", Url = "https://example.com/project1" },
+            new PortfolioItem { Title = "Project 2", Url = "https://example.com/project2" },
+            // Add more portfolio items as needed
+        };
+    }
+}
+
+public class PortfolioItem
+{
+    public string Title { get; set; }
+    public string Url { get; set; }
+}
